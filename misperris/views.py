e8d0dab793348d057ee70cbc5bdf9a47b0c39391
Gabriel Constenla <ga.constenla@alumnos.duoc.cl>
2018-10-20 17:10:53 -0300

    Navegacion de paginas con error
    Se logro la navegacion entre urls pero no cargan imagenes, carrusel y
    css

--- misperris/views.py
+++ misperris/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Adoptante, Adoptado
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'misperris/index.html', {})
+    adoptantes = Adoptante.objects.filter(fechaNacimiento=timezone.now()).order_by('fechaNacimiento')
+    adoptados = Adoptado.objects.order_by('nombre')
+    return render(request, 'misperris/index.html', {'adoptantes': adoptantes, 'adoptados': adoptados})
+
+def galeria(request):
+    return render(request, 'misperris/galeria.html',{})
+
+def adopta(request):
+    return render(request, 'misperris/adopta.html', {})
