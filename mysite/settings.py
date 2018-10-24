a543170647c6bd6ee4fca858f93fe4e005d487bd
Gabriel Constenla <ga.constenla@alumnos.duoc.cl>
2018-10-24 22:16:21 +0000

    Se agregó ruta MEDIA e instaló Pillow para imagen
    Se habilitó la funcion para añadir fotos en el panel de administración
    de django, pero no se pueden visualizar.

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,12 +127,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'gabrielconstenlaperris@gmail.com'
 EMAIL_HOST_PASSWORD = 'gabrielperris'
 EMAIL_PORT = 587
 
-MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
