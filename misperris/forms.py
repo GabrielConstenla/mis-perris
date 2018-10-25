3154ba934eaa094adff10980016824afa2cc1eb4
HectorSolaligue <h.solaligue@alumnos.duoc.cl>
2018-10-25 18:44:51 -0300

    Se agrego combobox y ahora valida por 2 en un def
    se agrego combobox para vivienda, region y ciudad y ahora si se pudo
    validar 2 cosas en un solo def

--- misperris/forms.py
+++ misperris/forms.py
@@ -8,21 +8,25 @@ class AdoptanteForm(forms.ModelForm):
 
     class Meta:
         model = Adoptante
-        fields = ('correo', 'run', 'nombreCompleto', 'telefono',)
+        fields = ('correo', 'run', 'nombreCompleto', 'telefono','region','ciudad','vivienda')
+
+    def clean_correo(self):
+        correo = self.cleaned_data['correo']
+
+        correo_base, proveedor = correo.split("@")
+        dominio, extension = proveedor.split(".")
+        if not dominio == "gmail":
+            raise ValidationError("Aqui se registra con gmail")
+        elif not extension == "com":
+            raise ValidationError("Aqui se registra con gmail.com")
+        return correo
+
 
     def clean_run(self):
         run = self.cleaned_data['run']
         if not "-" in run:
             raise ValidationError("Digita tu run sin puntos pero con guión")
-        # elif len(run.split()) <= 8:
-        #     raise ValidationError("Ingresa un rut con un minimo de 9 caracteres")
-        # if len(run.split()) <= 8:
-        #     raise ValidationError("Ingresa un rut con un minimo de 9 caracteres")
-        return run
-
-    def clean_largo_run(self):
-        run= self.cleaned_data['run']
-        if len(run.split()) <= 8:
+        elif len(run) <= 8:
             raise ValidationError("Ingresa un rut con un minimo de 9 caracteres")
         return run
 
@@ -30,7 +34,14 @@ class AdoptanteForm(forms.ModelForm):
         telefono = self.cleaned_data['telefono']
         if telefono.isalpha():
             raise ValidationError('El telefono no puede contener letras')
-        # if len(telefono.split()) <= 8:
-        #     raise ValidationError("Ingresa un telefono con un 9 digitos")
+        elif len(telefono) <= 8:
+            raise ValidationError("Ingresa un telefono con un 9 digitos")
         return telefono
+
+    # def clean_ciudades(self):
+    #     region=self.cleaned_data['region']
+    #     ciudad = self.cleaned_data['ciudad']
+    #     if region == "I de Tarapacá":
+    #         ciudad == "Arica"#, "Camarones", "Putre", "General Lagos"
+    #     return ciudad
   
