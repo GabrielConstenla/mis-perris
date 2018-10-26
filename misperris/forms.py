11e918554d566f2879b17a28d0e9f37481a2c5e3
HectorSolaligue <h.solaligue@alumnos.duoc.cl>
2018-10-25 22:04:26 -0300

    Se agrego validacion nombre y cambio mensajes
    se agrego una validacion para ingresar nombre y se cambiaron los
    mensajes de error

--- misperris/forms.py
+++ misperris/forms.py
@@ -8,7 +8,7 @@ class AdoptanteForm(forms.ModelForm):
 
     class Meta:
         model = Adoptante
-        fields = ('correo', 'run', 'nombreCompleto', 'telefono','region','ciudad','vivienda')
+        fields = ('correo', 'run', 'nombreCompleto', 'telefono','region','vivienda')
 
     def clean_correo(self):
         correo = self.cleaned_data['correo']
@@ -16,9 +16,9 @@ class AdoptanteForm(forms.ModelForm):
         correo_base, proveedor = correo.split("@")
         dominio, extension = proveedor.split(".")
         if not dominio == "gmail":
-            raise ValidationError("Aqui se registra con gmail")
+            raise ValidationError("Se tiene que registrar con un correo gmail.com")
         elif not extension == "com":
-            raise ValidationError("Aqui se registra con gmail.com")
+            raise ValidationError("Se tiene que registrar con un correo gmail.com")
         return correo
 
 
@@ -27,21 +27,23 @@ class AdoptanteForm(forms.ModelForm):
         if not "-" in run:
             raise ValidationError("Digita tu run sin puntos pero con guión")
         elif len(run) <= 8:
-            raise ValidationError("Ingresa un rut con un minimo de 9 caracteres")
+            raise ValidationError("Ingresa un run con un mínimo de 9 caracteres")
+        # elif run.isalpha() == "k":
+        #     raise ValidationError("Debe ingresar un run con k")
         return run
+
+    def clean_nombreCompleto(self):
+        nombreCompleto = self.cleaned_data['nombreCompleto']
+        if len(nombreCompleto.split(' ')) < 4 :
+            raise ValidationError("Por favor ingresa tu nombre completo")
+        return nombreCompleto
 
     def clean_telefono(self):
         telefono = self.cleaned_data['telefono']
         if telefono.isalpha():
-            raise ValidationError('El telefono no puede contener letras')
+            raise ValidationError('El teléfono no puede contener letras')
         elif len(telefono) <= 8:
-            raise ValidationError("Ingresa un telefono con un 9 digitos")
+            raise ValidationError("Ingresa un teléfono con un 9 digitos")
         return telefono
 
-    # def clean_ciudades(self):
-    #     region=self.cleaned_data['region']
-    #     ciudad = self.cleaned_data['ciudad']
-    #     if region == "I de Tarapacá":
-    #         ciudad == "Arica"#, "Camarones", "Putre", "General Lagos"
-    #     return ciudad
   
